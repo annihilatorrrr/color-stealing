1737808f41c97c32a69eea574c45057076db908f
davidsvy <zuzcus@gmail.com>
2022-04-17 05:37:23 +0300

    Update argument parser

--- run.py
+++ run.py
@@ -13,45 +13,64 @@ def parse_args():
     parser.add_argument(
         '--fixed', '-fi', type=str, default='')
     parser.add_argument(
-        '--resolution', '-r', type=int, default=2 ** 10)
+        '--resolution', '-r', type=int, default=2 ** 10,
+        help='Resolution of the created image. The image is square, so provide only one number.')
     parser.add_argument(
-        '--batch_size', '-b', type=int, default=128)
+        '--batch_size', '-b', type=int, default=128,
+        help='Number of points calculated at each iteration. '
+        'For fractal flame, should be <= 4 for better visuals.')
     parser.add_argument(
-        '--n_iter', '-i', type=int, default=int(5e4))
+        '--n_iter', '-i', type=int, default=int(5e4),
+        help='Number of iterations for the chaos game algorithm')
     parser.add_argument(
-        '--n_ignore', '-ni', type=int, default=int(2e2))
+        '--n_ignore', '-ni', type=int, default=int(2e2),
+        help='Number of initial iterations that will not be rendered.')
     parser.add_argument(
-        '--cmap', '-cm', type=str, default='i', choices=list(cmap_dict.keys()))
+        '--cmap', '-cm', type=str, default='i', choices=list(cmap_dict.keys()),
+        help='Matplotlib cmap for plotting/saving the image. Ignored if -f or -c is given.')
     parser.add_argument(
-        '--plot', '-p', action='store_true')
+        '--plot', '-p', action='store_true',
+        help='If given, the image will be rendered with plt.imshow.')
     parser.add_argument(
-        '--save', '-s', action='store_true')
+        '--save', '-s', nargs='?', const=True,
+        help='Usage: If not given, the image will not be saved.'
+        'If run as "-s", the image & IFS will be stored with the current timestamp as names.'
+        'If run as "-s <path>", the image will be stored as <path>.png and the IFS as <path>.csv.')
     parser.add_argument(
-        '--load', '-l', type=str, default='')
+        '--load', '-l', type=str, default='',
+        help='Path to stored csv IFS file. Usage: -l <path>.')
     parser.add_argument(
-        '--mutate', '-m', type=str, default='')
+        '--mutate', '-m', type=str, default='',
+        help='Path to stored csv IFS file that will be loaded & mutated. Usage: -m <path>.')
     parser.add_argument(
-        '--no_tqdm', '-nt', action='store_true')
+        '--no_tqdm', '-nt', action='store_true',
+        help='If given, the tqdm progress bar will not be used.')
     parser.add_argument(
-        '--dir', '-d', type=str, default='creations')
+        '--dir', '-d', type=str, default='creations',
+        help='Directory where the image & csv file will be stored.')
     parser.add_argument(
-        '--color_steal', '-c', type=str, default='')
+        '--color_steal', '-c', type=str, default='',
+        help='Path to an image that will be used for color stealing. Usage: -c <path>.')
     parser.add_argument(
-        '--flame', '-f', action='store_true')
+        '--flame', '-f', action='store_true',
+        help='If given, the Fractal Flame Algorithm will be used.')
     parser.add_argument(
-        '--gamma', '-g', type=float, default=20.)
+        '--gamma', '-g', type=float, default=20.,
+        help='Gamma parameter for the fractal frame algorithm.')
     parser.add_argument(
-        '--sup', '-su', type=int, default=3)
+        '--sup', '-su', type=int, default=3,
+        help='Supersampling ratio for the fractal flame algorithm.')
 
     args = parser.parse_args()
     args.cmap = cmap_dict[args.cmap]
 
-    assert args.save or args.plot
+    assert args.save or args.plot, 'Provide either -s or -p to run.'
     assert not (args.color_steal and args.flame), \
         'Cannot use both color stealing and fractal flame. Pick one.'
 
     if args.color_steal:
-        assert os.path.isfile(args.color_steal)
+        assert os.path.isfile(args.color_steal), \
+            f'"{args.color_steal}" is not a valid image.'
 
     if args.fixed:
         assert args.fixed in hardcoded.w
@@ -60,7 +79,8 @@ def parse_args():
     if args.mutate == 'r':
         args.mutate = most_recent_file(args)
 
-    assert args.gamma > 0 and args.sup >= 1
+    assert args.gamma > 0, 'Gamma must be positive.'
+    assert args.sup >= 1, 'Sup must be at least one.'
 
     return args
 
--- src/render.py
+++ src/render.py
@@ -20,22 +20,25 @@ def render_fractal(canvas, ifs, args):
         args (argparse.Namespace): Arguments for the fractal construction. See run.py
             for more details.
     """
+    img_dir = os.path.join(args.dir, img_subdir)
+    ifs_dir = os.path.join(args.dir, ifs_subdir)
+
+    if args.save is not None:
+        os.makedirs(img_dir, exist_ok=True)
+        os.makedirs(ifs_dir, exist_ok=True)
+
+        if args.save == True:
+            file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        else:
+            file_name = args.save
+        img_path = os.path.join(img_dir, f'{file_name}.png')
+        ifs_path = os.path.join(ifs_dir, f'{file_name}.csv')
+
+        plt.imsave(fname=img_path, arr=canvas, cmap=args.cmap)
+        save_ifs(ifs=ifs, path=ifs_path)
+
     if args.plot:
         plt.figure(figsize=(17, 17))
         plt.imshow(canvas, cmap=args.cmap)
         plt.axis('off')
         plt.show()
-
-    img_dir = os.path.join(args.dir, img_subdir)
-    ifs_dir = os.path.join(args.dir, ifs_subdir)
-
-    if args.save:
-        os.makedirs(img_dir, exist_ok=True)
-        os.makedirs(ifs_dir, exist_ok=True)
-
-        timestamp = datetime.datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
-        img_path = os.path.join(img_dir, f'{timestamp}.png')
-        ifs_path = os.path.join(ifs_dir, f'{timestamp}.csv')
-
-        plt.imsave(fname=img_path, arr=canvas, cmap=args.cmap)
-        save_ifs(ifs=ifs, path=ifs_path)
